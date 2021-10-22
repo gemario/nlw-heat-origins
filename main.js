b57b21d198565790ed00d0572955b3543f02d6cb
Jorge Mario <gmario.sh@gmail.com>
2021-10-22 01:17:51 -0300

    Projeto Finalizado
    Crachá virtual criado na trilha origin na NLW heat.

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'gemario',
+  youtube: 'channel/UCEZmF566-K73ql7eXduQsyg',
+  instagram: 'jorgee_mariobjj',
+  facebook: 'jorge.mario.330467',
+  twitter: 'geemarioo'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
